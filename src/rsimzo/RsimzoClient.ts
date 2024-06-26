9c62421cbe93228af63498f0c7f24816401d04df
jx_kurbanov <kurbanov.j.kh@gmail.com>
2024-06-26 14:49:12 +0500

    chore: remove test comment

--- src/rsimzo/RsimzoClient.ts
+++ src/rsimzo/RsimzoClient.ts
@@ -258,8 +258,6 @@ export class RsimzoClient extends Hookable<HookTypes> {
     return res.token
   }
 
-  //test commit
-
   public cleanup(iframe: HTMLIFrameElement): void {
     if (iframe && iframe.parentElement) {
       iframe.parentElement.removeChild(iframe)
