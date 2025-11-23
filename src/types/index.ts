e3e12bc2f81da3eb0ba4c4b541146491d92206d0
jx_kurbanov <kurbanov.j.kh@gmail.com>
2025-11-23 20:39:59 +0500

    refactor: transfer type to root index

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,55 @@
-export * from './RsimzoClient'
+export type RsAction = 'signature_list' | 'sign' | 'ready' | 'parse_pkcs7' | 'certificates_info' | 'auth'
+export type Locale = 'ru' | 'en' | 'uz'
+export interface RsOptions {
+  locale?: Locale
+  publicKey: string
+  targetOrigin?: string
+}
+
+export type RsResolvedOptions =
+  Omit<Required<RsOptions>, 'publicKey'> & Pick<RsOptions, 'publicKey'>
+
+export interface RsSignOptions {
+  locale?: string
+  attached?: boolean
+}
+
+export interface RsAuthOptions {
+  locale?: string
+}
+
+export interface RsCertificatesOptions {
+  locale?: string
+}
+
+export interface RsSignatureInfo {
+  serial: string
+  fullName: string
+  createdAt: number
+  expireAt: number
+  country: string
+  orgName?: string
+  pin?: string // Personal Identification Number
+  tin?: string
+  isLegalEntity: boolean
+  isExpired: boolean
+}
+
+export interface RsPostMessageResult<T> {
+  error?: RsPostMessageError | null
+  data?: T | null
+  method?: 'storage_access_prompt'
+  success?: boolean
+}
+
+export interface RsPostMessageError {
+  errorCode: number;
+  errorMessage: string;
+  rawError?: any
+}
+
+export interface BuildUrlOptions {
+  path: string
+  params?: Record<string, string>
+  query?: Record<string, string>
+}
